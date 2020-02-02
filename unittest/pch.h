6808005bfb95ddd98c3b81a58b31012e9be9e05a
Victor Derks <vbaderks@gmail.com>
2020-02-02 21:18:49 +0100

    Disable the warning that 5204 is not a warning
    Older MSVC C++ compiler don't know new 5204 warning, disable the warning about about unknown warning numbers.

--- unittest/pch.h
+++ unittest/pch.h
@@ -12,6 +12,7 @@
 #include <unknwn.h>
 
 #pragma warning(push)
+#pragma warning(disable : 4619) // #pragma warning : there is no warning number '5204'
 #pragma warning(disable : 5204) // class has virtual functions, but its trivial destructor is not virtual
 #include <winrt/base.h>
 #pragma warning(pop)
