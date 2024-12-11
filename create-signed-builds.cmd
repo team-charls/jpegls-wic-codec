a5a23e015b8aa521a1959cf9c42b0221a57c4ad5
Victor Derks <vbaderks@gmail.com>
2024-12-11 15:05:44 +0100

    Correct the output name of the installer

--- create-signed-builds.cmd
+++ create-signed-builds.cmd
@@ -4,15 +4,15 @@
 :: Build signed x86
 msbuild -t:clean -p:Configuration=Release -p:Platform=Win32
 msbuild -t:restore -p:Configuration=Release -p:Platform=Win32
-msbuild -t:build -p:Configuration=Release -p:Platform=Win32 -p:CHARLS_DETERMINISTIC_BUILD=true -p:SignOutput=true -p:CertificateThumbprint=%1 -p:TimestampUrl=%2
+msbuild -t:build -p:ContinuousIntegrationBuild=true -p:Configuration=Release -p:Platform=Win32 -p:CHARLS_DETERMINISTIC_BUILD=true -p:SignOutput=true -p:CertificateThumbprint=%1 -p:TimestampUrl=%2
 
 :: Build signed x64
 msbuild -t:clean -p:Configuration=Release -p:Platform=x64
 msbuild -t:restore -p:Configuration=Release -p:Platform=x64
-msbuild -t:build -p:Configuration=Release -p:Platform=x64 -p:CHARLS_DETERMINISTIC_BUILD=true -p:SignOutput=true -p:CertificateThumbprint=%1 -p:TimestampUrl=%2
+msbuild -t:build -p:ContinuousIntegrationBuild=true -p:Configuration=Release -p:Platform=x64 -p:CHARLS_DETERMINISTIC_BUILD=true -p:SignOutput=true -p:CertificateThumbprint=%1 -p:TimestampUrl=%2
 
 :: Build signed ARM64
 msbuild -t:clean -p:Configuration=Release -p:Platform=ARM64
 msbuild -t:restore -p:Configuration=Release -p:Platform=ARM64
-msbuild -t:build -p:Configuration=Release -p:Platform=ARM64 -p:CHARLS_DETERMINISTIC_BUILD=true -p:SignOutput=true -p:CertificateThumbprint=%1 -p:TimestampUrl=%2
+msbuild -t:build -p:ContinuousIntegrationBuild=true -p:Configuration=Release -p:Platform=ARM64 -p:CHARLS_DETERMINISTIC_BUILD=true -p:SignOutput=true -p:CertificateThumbprint=%1 -p:TimestampUrl=%2
 
